2134758a846d8b74999abc31b15c2e167333f7bd
Denis <carriere.denis@gmail.com>
2018-04-14 17:45:23 -0400

    Update bios scripts

--- scripts/6-transfer-eos-tokens.sh
+++ scripts/6-transfer-eos-tokens.sh
@@ -1,6 +1,28 @@
 #!/bin/bash
 
-# EOS Scholar - Transfer EOS Tokens
+# EOS Scholar - Initial Transfer EOS Tokens
 # https://github.com/ScholarTestnet
 
-cleos transfer eosio eos.nation "1000000.0000 EOS" "Transfer EOS"
+cleos transfer eosio denis.c 434782608696 "Initial transfer from eosio to denis.c"
+cleos transfer eosio heipacker 434782608696 "Initial transfer from eosio to heipacker"
+cleos transfer eosio stuardo.str 434782608696 "Initial transfer from eosio to stuardo.str"
+cleos transfer eosio wuyahuang 434782608696 "Initial transfer from eosio to wuyahuang"
+cleos transfer eosio bitcoin.eos 434782608696 "Initial transfer from eosio to bitcoin.eos"
+cleos transfer eosio bitspace 434782608696 "Initial transfer from eosio to bitspace"
+cleos transfer eosio coinstakes 434782608696 "Initial transfer from eosio to coinstakes"
+cleos transfer eosio crypto.lions 434782608696 "Initial transfer from eosio to crypto.lions"
+cleos transfer eosio dac.gac 434782608696 "Initial transfer from eosio to dac.gac"
+cleos transfer eosio cafe.calgary 434782608696 "Initial transfer from eosio to cafe.calgary"
+cleos transfer eosio eos.cannon 434782608696 "Initial transfer from eosio to eos.cannon"
+cleos transfer eosio eos.detroit 434782608696 "Initial transfer from eosio to eos.detroit"
+cleos transfer eosio eos.dublin 434782608696 "Initial transfer from eosio to eos.dublin"
+cleos transfer eosio eos.nairobi 434782608696 "Initial transfer from eosio to eos.nairobi"
+cleos transfer eosio eos.nation 434782608696 "Initial transfer from eosio to eos.nation"
+cleos transfer eosio eos.new.york 434782608696 "Initial transfer from eosio to eos.new.york"
+cleos transfer eosio eos.sydney 434782608696 "Initial transfer from eosio to eos.sydney"
+cleos transfer eosio eos.union 434782608696 "Initial transfer from eosio to eos.union"
+cleos transfer eosio eos.wtz 434782608696 "Initial transfer from eosio to eos.wtz"
+cleos transfer eosio eos42 434782608696 "Initial transfer from eosio to eos42"
+cleos transfer eosio eosocal 434782608696 "Initial transfer from eosio to eosocal"
+cleos transfer eosio meet.one 434782608696 "Initial transfer from eosio to meet.one"
+cleos transfer eosio worbli 434782608696 "Initial transfer from eosio to worbli"
